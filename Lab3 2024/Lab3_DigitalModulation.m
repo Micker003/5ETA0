b2f85426e1fb9bfec65f4fc56184020e81984402
Micker003 <43570163+Micker003@users.noreply.github.com>
2024-03-28 12:12:59 +0100

    final proj 3

--- Lab3 2024/Lab3_DigitalModulation.m
+++ Lab3 2024/Lab3_DigitalModulation.m
@@ -6,20 +6,20 @@
 
 %% 2nd section 
 % Run this section when making different levels for QAM.
-
-binary = audioToBinary(wav, [ ]);  % insert number of bits corresponding to 
+ 
+binary = audioToBinary(wav, [7 ]);  % insert number of bits corresponding to 
                                    % the modulation levels you want to 
                                    % achieve
 
 %% 3rd section
   
-% BER = [ ];        %store your BER values here as a vector
-% t = [ ];          %store your t values here as a vector
+BER = [0,0,0,0.000102,0.006515, 0.07883, 0.2516,0.4945  ];        %store your BER values here as a vector
+t = [181.374, 90.624875, 60.374875, 36.2498, 30.124875, 25.874, 22.624  ];          %store your t values here as a vector
 
-% figure
-% subplot(2,1,1);
-% plot(BER);
-% ylabel('BER');
-% subplot(2,1,2);
-% plot(t);
-% ylabel('t [s]');
+figure
+subplot(2,1,1);
+plot(BER);
+ylabel('BER');
+subplot(2,1,2);
+plot(t);
+ylabel('t [s]');
